c5af86e01d625d2b5869ccee635f03fffa0df71c
Yunkyu Lee <gtv1523411@gmail.com>
2019-10-05 16:52:39 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [count] = useState(0);
+  const [data, setData] = useState(null)
+
+   useEffect(() => {
+     async function fetchData() {
+       console.log(data);
+       console.log(axios)
+       let result = await axios.get('https://urlreq.appspot.com/req?method=GET&url=https://github.com/users/Younkyu/contributions');
+       console.log(result.data)
+       console.log(typeof result.data)
+       const html = result.data
+       (new DOMParser).parseFromString(html, "text/html")
+       setData(5)
+       console.log(data)
+     }
+
+     fetchData()
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {data}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
